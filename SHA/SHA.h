57738c304a7f62500d5a80375fe5da5b2e9dfa89
Kenneth Durbrow <kmdurbrow@me.com>
2017-10-17 21:37:51 -0400

    Initial Commit

---
+++ SHA/SHA.h
@@ -0,0 +1,19 @@
+//
+//  SHA.h
+//  SHA
+//
+//  Created by Kenneth Durbrow on 10/17/17.
+//  Copyright © 2017 Kenneth M. Durbrow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SHA.
+FOUNDATION_EXPORT double SHAVersionNumber;
+
+//! Project version string for SHA.
+FOUNDATION_EXPORT const unsigned char SHAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHA/PublicHeader.h>
+
+
